f529cd88c07d9b39273e221fcfd7a30166de9bca
Adam Tiller <a.j.tiller@gmail.com>
2021-12-06 12:17:25 -0500

    Refactor to generator.

--- day06.js
+++ day06.js
@@ -1,42 +1,30 @@
-
 const fs = require('fs');
 
 const input = fs.readFileSync(__dirname + '/inputs/06.input', 'utf8').trim().split(',').map(Number);
 
-const simulationInput = input.slice(0);
-for (let i = 0; i < 80; i++) {
-  advance(simulationInput);
+const DAYS_TO_PRINT = [80, 256];
+const NEW_SPAWN_DAY = 8;
+const POST_SPAWN_DAY = 6;
+
+let population;
+const populationTracker = fishProgress(input.reduce((fishState, fishDay) => {
+  fishState[fishDay]++;
+  return fishState;
+}, new Array(NEW_SPAWN_DAY + 1).fill(0)));
+
+for (let i = 1; i < 257; i++) {
+  population = populationTracker.next();
+  if (DAYS_TO_PRINT.includes(i)) console.log(`On day ${i} there are ${population.value} fish.`);
 }
 
-console.log(`There are ${simulationInput.length} lanternfish at 80 days.`);
+function* fishProgress(initialState) {
+  const currentState = initialState.slice(0);
 
-function advance(fishList) {
-  const newFish = [];
-
-  for (let i = 0; i < fishList.length; i++) {
-    if (fishList[i] === 0) {
-      fishList[i] = 6;
-      newFish.push(8);
-    } else {
-      fishList[i]--;
-    }
+  while(true) {
+    const spawned = currentState.shift();
+    currentState.push(spawned);
+    currentState[POST_SPAWN_DAY] += spawned;
+    yield currentState.reduce((a, v) => a + v);
   }
-
-  fishList.push(...newFish);
 }
 
-
-let fishCounts = new Array(9).fill(0);
-for (const fish of input) {
- fishCounts[fish]++;
-}
-
-for (let i = 0; i < 256; i++) {
-  fishCounts = rotationalAdvance(fishCounts);
-}
-
-console.log(`There are ${fishCounts.reduce((a, v) => a + v)} lanternfish at 256 days.`);
-
-function rotationalAdvance([zero, one, two, three, four, five, six, seven, eight]) {
-  return [one, two, three, four, five, six, seven + zero, eight, zero];
-}
